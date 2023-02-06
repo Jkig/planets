e221d4e9d09422d1a33ca72cb609f5df7646c25e
Jkig <DerekLeroux47@gmail.com>
2023-02-06 05:48:15 -0700

    background, and a little orbit prep

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import * as THREE from "three"
 import "./style.css"
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import jupiterImage from '/img/2k_jupiter.jpg';
+import galaxy from '/img/big_galaxy.jpg';
 
 
 const sizes = {
@@ -23,15 +24,24 @@ let sunSize = 1400000;
 const scene = new THREE.Scene();
 //    jupiter
 const textureLoader = new THREE.TextureLoader();
+
+const outside = new THREE.SphereGeometry(distanceFromSun*15, 64, 64);
+const outsideMat = new THREE.MeshStandardMaterial({
+  map: textureLoader.load(galaxy),
+  side: THREE.DoubleSide,
+});
+const fullOutside = new THREE.Mesh(outside,outsideMat);
+scene.add(fullOutside)
+
+
 const jupiter = new THREE.SphereGeometry(jupiterSize, 64, 64, );
 const jupiterTexture = new THREE.MeshStandardMaterial({
   // color: '#FF8C00',
   map: textureLoader.load(jupiterImage)
 });
-
-
-
 const meshJupiter = new THREE.Mesh(jupiter, jupiterTexture);
+meshJupiter.rotation.z = 0.05462881
+
 scene.add(meshJupiter);
 //    sun
 const sun = new THREE.SphereGeometry(sunSize, 64, 64, );
@@ -45,9 +55,12 @@ meshJupiter.position.x = distanceFromSun;
 
 
 // Light
-const light = new THREE.PointLight(0xFDB813, 1);
-light.position.set(5000, 0, 0 );// TODO this as part of set up for movement
+const light = new THREE.PointLight(0xFDB813, 1.7, distanceFromSun*2);
+//light.position.set(0, 0, 0 );// TODO this as part of set up for movement
 scene.add(light);
+
+const backgroundLight = new THREE.PointLight(0xFFFFFF, .15)
+scene.add(backgroundLight)
 
 // camera (titan)
 const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 1, 1778000000);
@@ -57,16 +70,25 @@ scene.add(camera);
 
 
 const orbit = new OrbitControls(camera,renderer.domElement);
-const posiotion_center = new THREE.Vector3( distanceFromSun, 0, 0 );
-orbit.target = posiotion_center;
+// const posiotion_center = new THREE.Vector3( distanceFromSun, 0, 0 );
+//orbit.target = posiotion_center;
+orbit.target = meshJupiter.position
+
 
 orbit.update();
 
-// animate turn
+// animate turn and orbit
 function animate(time) {
   meshJupiter.rotation.y = time / 1000;
   renderer.render(scene, camera);
   orbit.update();
+
+  /*
+  meshJupiter.position.z = distanceFromSun*Math.cos(time/2000)
+  meshJupiter.position.x = distanceFromSun*Math.sin(time/2000)
+  orbit.target = meshJupiter.position
+  */
+
 };
 renderer.setAnimationLoop(animate);
 
