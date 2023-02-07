0a5450bc741c8417e792b7d48b1424fdae5d8c11
Jkig <DerekLeroux47@gmail.com>
2023-02-07 04:43:17 -0700

    got first person working, still a bit odd

--- main.js
+++ main.js
@@ -1,8 +1,41 @@
 import * as THREE from "three"
 import "./style.css"
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { FirstPersonControls } from "../planets/FirstPersonControls";
+
+//import * as dat from "dat.gui";
+
 import jupiterImage from '/img/2k_jupiter.jpg';
 import galaxy from '/img/big_galaxy.jpg';
+
+// TODO:
+/*
+* the background moves relavtive to planets, which obv. looks weird
+- this may solve itself when the camera orbits, we can't see the sky
+* spacebar or something to toggle look around, its weird????? idk yet
+*/
+
+const clock = new THREE.Clock();
+
+let speed = .1
+// master speed scales eveything, so other values can be ported in ezier
+// time values
+// all in seconds
+let jupiterDayLength_preScale = 35609
+let jupiterYearLength_preScale = 374371200
+let europaOrbitLenght_preScale = 306000
+// europa is tidally locked
+// post scale:
+let jupiterDayLength = jupiterDayLength_preScale*speed
+let jupiterYearLength = jupiterYearLength_preScale*speed
+let europaOrbitLenght = europaOrbitLenght_preScale*speed
+
+
+// all units in km, i'll see if that needs to be optimized...
+let distanceFromSun = 778000000;
+let jupiterSize = 69911;
+let europaOrbit = 671000;
+let sunSize = 1400000;
 
 
 const sizes = {
@@ -15,11 +48,6 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(5);
 document.body.appendChild(renderer.domElement)
 
-// all units in km, i'll see if that needs to be optimized...
-let distanceFromSun = 778000000;
-let jupiterSize = 69911;
-let europaOrbit = 671000;
-let sunSize = 1400000;
 
 const scene = new THREE.Scene();
 //    jupiter
@@ -69,25 +97,35 @@ camera.position.x = distanceFromSun;
 scene.add(camera);
 
 
-const orbit = new OrbitControls(camera,renderer.domElement);
-// const posiotion_center = new THREE.Vector3( distanceFromSun, 0, 0 );
+const orbit = new FirstPersonControls(camera,renderer.domElement);
+orbit.movementSpeed = 0;
+orbit.lookSpeed = .1
+const posiotion_center = new THREE.Vector3( distanceFromSun, 0, 0 );
 //orbit.target = posiotion_center;
-orbit.target = meshJupiter.position
-
-
-orbit.update();
+//orbit.target = meshJupiter.position
+orbit.update(clock.getDelta());
 
 // animate turn and orbit
 function animate(time) {
-  meshJupiter.rotation.y = time / 1000;
-  renderer.render(scene, camera);
-  orbit.update();
 
-  /*
-  meshJupiter.position.z = distanceFromSun*Math.cos(time/2000)
-  meshJupiter.position.x = distanceFromSun*Math.sin(time/2000)
+  /* only if testing with gui??*/
+  jupiterDayLength = jupiterDayLength_preScale*speed
+  jupiterYearLength = jupiterYearLength_preScale*speed
+  europaOrbitLenght = europaOrbitLenght_preScale*speed
+
+
+  meshJupiter.rotation.y = time / jupiterDayLength;
+  
+  meshJupiter.position.z = distanceFromSun*Math.cos(time/jupiterYearLength)
+  meshJupiter.position.x = distanceFromSun*Math.sin(time/jupiterYearLength)
+
+  camera.position.z = meshJupiter.position.z+europaOrbit*Math.cos(time/europaOrbitLenght);
+  camera.position.x = meshJupiter.position.x+europaOrbit*Math.sin(time/europaOrbitLenght);
   orbit.target = meshJupiter.position
-  */
+
+
+  orbit.update(clock.getDelta());
+  renderer.render(scene, camera);
 
 };
 renderer.setAnimationLoop(animate);
